b369b3bc6e858a8a36e022228f0a8b8479401af4
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-04-03 15:48:43 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 
